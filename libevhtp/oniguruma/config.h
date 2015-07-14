da8761531ccfd408108f52cf2b86bf430457d3bd
chjp2046 <chjp2046@126.com>
2015-07-14 18:37:38 +0800

    rename libevhtp-dev

---
+++ libevhtp/oniguruma/config.h
@@ -0,0 +1,34 @@
+/* #undef CRAY_STACKSEG_END */
+/* #undef C_ALLOCA */
+/* #undef HAVE_ALLOCA */
+#define HAVE_ALLOCA_H 1
+/* #undef HAVE_DLFCN_H */
+/* #undef HAVE_INTTYPES_H */
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_PROTOTYPES */
+#define HAVE_STDARG_PROTOTYPES 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+/* #undef HAVE_SYS_STAT_H */
+#define HAVE_SYS_TIMES_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+/* #undef LT_OBJDIR */
+/* #undef PACKAGE */
+/* #undef PACKAGE_BUGREPORT */
+/* #undef PACKAGE_NAME */
+/* #undef PACKAGE_STRING */
+/* #undef PACKAGE_TARNAME */
+/* #undef PACKAGE_VERSION */
+#define SIZEOF_INT 1
+#define SIZEOF_LONG 1
+#define SIZEOF_SHORT 1
+/* #undef STACK_DIRECTION */
+/* #undef STDC_HEADERS */
+/* #undef TIME_WITH_SYS_TIME */
+/* #undef USE_COMBINATION_EXPLOSION_CHECK */
+/* #undef USE_CRNL_AS_LINE_TERMINATOR */
+/* #undef VERSION */
